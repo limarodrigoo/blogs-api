62c0b6ef583e6f995444bf1f985069f60cfd6986
Rodrigo Lima <limarodrigoo@outlook.com>
2022-03-11 21:09:53 -0300

    :bento: creating sequelize setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userRouter = require('./router/userRouter');
 
 const app = express();
 
@@ -8,3 +9,6 @@ app.listen(3000, () => console.log('ouvindo porta 3000!'));
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(express.json());
+app.use('/user', userRouter);
