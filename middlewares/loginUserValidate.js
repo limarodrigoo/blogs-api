273337b46161e4e18b14535e22d1f25a4c0353da
Rodrigo Lima <limarodrigoo@outlook.com>
2022-03-14 01:51:30 -0300

    :adhesive_bandage: remove unnecessary async

--- middlewares/loginUserValidate.js
+++ middlewares/loginUserValidate.js
@@ -15,12 +15,12 @@ const schema = Joi.object({
     }),
 });
 
-const loginUserValidate = async (req, res, next) => {
+const loginUserValidate = (req, res, next) => {
   try {
     const { email, password } = req.body;
     const { error } = schema.validate({ email, password });
     if (error) {
-      const { message } = error.details[0] || error;
+      const { message } = error.details[0];
       return res.status(400).json({ message });
     }
     next();
